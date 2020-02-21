f01a270046a76147cdbe9db712bb31765936b33f
lcwhzr <lcwhazard@gmail.com>
2020-02-21 17:24:47 +0100

    init folders

--- Services/Service.cs
+++ Services/Service.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenTheDoor
+namespace OpenTheDoor.Services
 {
-    public class Class1
+    class Service
     {
     }
 }
