b684e125b98dc93b07bf997e0dfefc2f0a9f2b7d
JohanSing <singainy.johan@protonmail.com>
2020-03-05 12:32:32 +0100

    Enhance client model

---
+++ Models/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OpenTheDoor.Models
+{
+    class Client
+    {
+        public String Name { get; }
+        public String Identifier { get; }
+        public String Secret { get; }
+        public Array RedirectURIs { get; set; }
+        public Array Grants { get; set; }
+        public Array Scopes { get; set; }
+        public Boolean Active { get; set; }
+
+        public Client(string clientName, string identifierClient, string clientSecret)
+        {
+            Name = clientName;
+            Identifier = identifierClient;
+            Secret = clientSecret;
+            Active = true;
+        }
+
+        public Boolean IsActive()
+        {
+            return Active;
+        }
+    }
+}
