06d29c2411c799819b69764e5bc8c08456122055
JohanSing <singainy.johan@protonmail.com>
2020-03-05 17:55:24 +0100

    Update each models and create new model AccessToken

---
+++ Models/Scope.cs
@@ -0,0 +1,17 @@
+﻿namespace OpenTheDoor.Models
+{
+    class Scope
+    {
+        private string ScopeName;
+
+        public Scope(string scope)
+        {
+            ScopeName = scope;
+        }
+
+        public string GetScopeName()
+        {
+            return ScopeName;
+        }
+    }
+}
---
+++ Models/Service.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenTheDoor.Models
+{
+    class Service
+    {
+        private string Name;
+        private string ApiKey;
+        private Boolean Active;
+        private List<Scope> Scopes;
+
+        public Service(string clientName, string apiKey)
+        {
+            Name = clientName;
+            ApiKey = apiKey;
+            Active = true;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetApiKey()
+        {
+            return ApiKey;
+        }
+
+        public Boolean IsActive()
+        {
+            return Active;
+        }
+
+        public void Desactive()
+        {
+            Active = false;
+        }
+
+        public List<Scope> GetScopes()
+        {
+            return Scopes;
+        }
+    }
+}
