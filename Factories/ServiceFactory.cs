22729bb9d976975c746ec897c627a299a472d5ca
JohanSing <singainy.johan@protonmail.com>
2020-03-06 15:31:17 +0100

    Update each models Create factory to create service Create exception to verify if scope for service is valid

---
+++ Factories/ServiceFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenTheDoor.Models;
+using OpenTheDoor.Exceptions;
+
+namespace OpenTheDoor.Factories
+{
+    class ServiceFactory
+    {
+        public Service CreateService(string domainName, Scope scope = null)
+        {
+            Service service = new Service(domainName, Guid.NewGuid().ToString());
+
+            if (null != scope)
+            {
+                try
+                {
+                    ScopeKeyService.KeyInScopeKeys(scope);
+                }
+                catch(InvalidScopeServiceException exception)
+                {
+                    throw exception;
+                }
+
+                service.SetScope(scope);
+            }
+
+            return service;
+        }
+    }
+}
