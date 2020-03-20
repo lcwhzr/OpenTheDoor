353ad68c1365ce0b72431c9f646df69b76e1c836
JohanSing <singainy.johan@protonmail.com>
2020-03-20 15:59:00 +0100

    Update AccessTokenCreator to add condition on service and scope and to throw exception when its invalid

---
+++ Core/Exceptions/ServiceNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Core.Exceptions
+{
+    [Serializable]
+    class ServiceNotFoundException : Exception
+    {
+        public ServiceNotFoundException(int serviceId)
+            : base(String.Format("Service Not Found With This ID: {0}", serviceId))
+        {
+        }
+    }
+}
